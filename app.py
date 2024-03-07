bb566eb729befc01780da7d308757366e4eb0b6c
felixandres1 <felix.agosto@bts.tech>
2024-03-07 03:08:41 +0100

    Streamlit additions

--- app.py
+++ app.py
@@ -30,21 +30,51 @@ def main():
             make_headless = True
             scraping(make_headless,input1,"product1_amazon")
             scraping(make_headless,input2,"product2_amazon")
-
             df1=pd.read_excel("output/product1_amazon.xlsx")
+            df1.reset_index(drop=True, inplace=True)
+
             df2=pd.read_excel("output/product2_amazon.xlsx")
-            st.table(df1.head(5))
-            st.table(df2.head(5))
+            df2.reset_index(drop=True, inplace=True)
+
+            col1, col2 = st.columns(2)
+            col1.subheader(f"{input1}")
+            col1.image(df1.loc[1, 'Imagen'])
+            col1.subheader(df1.loc[1, 'Precio'])
+            col1.subheader(df1.loc[1, 'Calificacion'])
+            col1.image("https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", width=150)
+            col1.dataframe(df1.head(5))
+
+            col2.subheader(f"{input1}")
+            col2.image(df2.loc[1, 'Imagen'])
+            col2.subheader(df1.loc[1, 'Precio'])
+            col2.subheader(df1.loc[1, 'Calificacion'])
+            col2.image("https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", width=150)
+            col2.dataframe(df2.head(5))
 
             scrapper("--headless", input1, "product1_ebay")
             scrapper("--headless", input2, "product2_ebay")
             df3=pd.read_excel("output/product1_ebay.xlsx")
+            df3.reset_index(drop=True, inplace=True)
+
             df4=pd.read_excel("output/product2_ebay.xlsx")
-            st.table(df3.head(5))
-            st.table(df4.head(5))
+            df4.reset_index(drop=True, inplace=True)
+
+            col1.subheader(f"{input1}")
+            col1.image(df3.loc[1, 'Image'])
+            col1.subheader(df3.loc[1, 'Price'])
+            col1.subheader(df3.loc[1, 'Condition'])
+            col1.image("https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", width=150)
+            col1.dataframe(df3.head(5))
+
+            col2.subheader(f"{input2}")
+            col2.image(df4.loc[1, 'Image'])
+            col2.subheader(df4.loc[1, 'Price'])
+            col2.subheader(df4.loc[1, 'Condition'])
+            col2.dataframe(df4.head(5))
 
         else:
             st.write("Please enter two products to compare.")
 
 if __name__ == "__main__":
+    #show_ebay_section = st.sidebar.checkbox("Show eBay Section")
     main()
--- eBay_testing.py
+++ eBay_testing.py
@@ -15,11 +15,26 @@ def main():
             df1 = scrapper("--headless", product_1, "FILE")
             df2 = scrapper("--headless", product_2, "FILE")
             st.success("eBay scraped successfully for both products!")
-            st.columns(2)
-            col1.dataframe(df1.head(3))
-            col1.image(df1.loc[1, 'Image'])
-            col2.dataframe(df2.head(3))
-            col2.image(df2.loc[1, 'Image'])
+            st.header(f"{product_1} vs {product_2}")
+            st.write("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+            c1, c2 = st.columns(2)
+        # PROD 1
+            c1.subheader(f"{product_1}")
+            c1.image(df1.loc[1, 'Image'])
+            # first price
+            c1.subheader(df1.loc[1, 'Price'])
+            c1.subheader(df1.loc[1, 'Condition'])
+            c1.image("https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg", use_column_width=True)
+            c1.dataframe(df1.head(3))
+        # PROD 2
+            c2.subheader(f"{product_2}")
+            c2.image(df2.loc[1, 'Image'])
+            # first price
+            c2.subheader(df2.loc[1, 'Price'])
+            c2.subheader(df2.loc[1, 'Condition'])
+            c2.image("https://upload.wikimedia.org/wikipedia/commons/1/1b/EBay_logo.svg", use_column_width=True)
+            c2.dataframe(df2.head(3))
+
 
             # st.write(f"Product {product_name} results in eBay")
             # st.dataframe()
